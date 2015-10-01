081c5b1aaa10eef2566cc5862e546a0b511a5b77
Сергей Попов <tovarov.piter@gmail.com>
2015-10-01 20:05:29 +0300

    Подключил все необходимые шрифты

--- Gruntfile.js
+++ Gruntfile.js
@@ -117,7 +117,7 @@ module.exports = function(grunt) {
       fonts: {
         expand: true,
         cwd: 'src/font/',
-        src: ['*.{eot,svg,woff,ttf}'],
+        src: ['*.{eot,svg,woff,woff2,ttf}'],
         dest: 'build/font/',
       },
     },
