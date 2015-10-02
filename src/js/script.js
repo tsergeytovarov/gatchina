b3a4e7acd204913d552fa50941bfe9cbfe6c079e
Сергей Попов <tovarov.piter@gmail.com>
2015-10-02 17:56:54 +0300

    Скрипт скролинга

--- src/js/script.js
+++ src/js/script.js
@@ -12,4 +12,12 @@ $(function() {
     $(this).parents(".time-screen").css("background-image", "url(img/" + $(this).attr("data-image") + ")");
   });
 
+  $(".js-scroll").on("click", function(event){
+    event.preventDefault;
+    var section = $(this).attr("data-scroll");
+    $('html, body').animate({
+        scrollTop: $("." + section).offset().top
+    }, 1000);
+  });
+
 });
