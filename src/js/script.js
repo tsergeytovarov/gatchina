aab5a8c9214737185608eb3dddf9726a67ce8f2b
Сергей Попов <tovarov.piter@gmail.com>
2015-10-03 15:43:12 +0300

    11 часов и скрипт переключения фотографий

--- src/js/script.js
+++ src/js/script.js
@@ -22,4 +22,14 @@ $(function() {
     $(this).addClass("navigation__link--active");
   });
 
+  //обработка картинок
+  $(".photo-carousel__preview").on('click', function(){
+    var this_number = $(this).attr("data-image");
+    var this_photo = "photo-carousel__preview--" + this_number;
+    var father = $(this).parents(".photo-carousel");
+    father.find(".photo-carousel__active").removeClass("photo-carousel__active").addClass("photo-carousel__preview").addClass(this_photo).attr("data-image", this_number);
+    $(this).removeClass("photo-carousel__preview").removeClass(this_photo).addClass("photo-carousel__active").attr("data-image", "0");
+
+  });
+
 });
